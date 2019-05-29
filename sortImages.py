7bf20e9fdaf363954c326cd48a7b6c31abe86172
Jas10022 <jas10022jb@gmail.com>
2019-05-28 23:04:15 -0700

    data_split

--- sortImages.py
+++ sortImages.py
@@ -2,6 +2,8 @@
 
 import json
 import os
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 # open open category to id file
 with open('val2019.json') as f:
@@ -10,6 +12,28 @@ with open('val2019.json') as f:
 #open id to images file
 with open('train2019.json') as f:
     images = json.load(f)
+
+
+annotation_images = pd.DataFrame.from_dict(images["annotations"])
+catagory_images = pd.DataFrame.from_dict(images["categories"])
+images_id = pd.DataFrame.from_dict(images["images"])
+
+train_data = pd.DataFrame({'ImageId':None,'Class':None,'Family':None,'Genus':None,'Kingdom':None,'Name':None,'Order':None,'Phylum':None}, index = [0])
+    
+index = 0;
+for t in annotation_images.category_id:
+    current_info = pd.DataFrame(catagory_images.loc[t]).T
+    
+    train_data.loc[index] = [index, current_info["class"], current_info["family"], current_info["genus"], current_info["kingdom"], current_info["name"], current_info["order"], current_info["phylum"]]
+    print(index)
+    index += 1
+    
+labelencoder = LabelEncoder()
+train_data[:, 1] = train_data.fit_transform(x[:, 1])
+
+lower_train_data = pd.DataFrame({'FileName': images_id['file_name'],'ImageId': annotation_images['id'], 'CatagoryID':annotation_images['category_id']})
+
+lower_train_data = lower_train_data.sort_values(by=['CatagoryID'])
 
 annotations = data["annotations"]
 #get path of file
