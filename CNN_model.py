716f10f32495def1497f22d3f03854a12d867b50
Jas10022 <jas10022jb@gmail.com>
2019-05-31 20:57:08 -0700

    update
    must add a "upper", or "lower" tag to determine which model to create

--- CNN_model.py
+++ CNN_model.py
@@ -16,7 +16,7 @@ from PIL import Image
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
-def CNN(train_data, train_labels, eval_data, eval_labels, output_nodes_number, model_name):
+def CNN(train_data, train_labels, eval_data, eval_labels, output_nodes_number, model_name, model_type):
     
     #convert images into 28 x 28 pictures
     train_data = train_data.values.reshape((-1, 28, 28, 1))
@@ -35,135 +35,137 @@ def CNN(train_data, train_labels, eval_data, eval_labels, output_nodes_number, m
     # Convert `images28` to grayscale
     eval_data = rgb2gray(eval_data)
     
-    #tensorflow model function for lowerNN
-    def cnn_model(features, labels, mode):
-        
-        input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])
-        
-        # Convolutional Layer #1
-        conv1 = tf.layers.conv2d(
-              inputs=input_layer,
-              filters=32,
-              kernel_size=[5, 5],
-              padding="same",
-              activation=tf.nn.relu)
-        
-          # Pooling Layer #1
-        pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
-        
-          # Convolutional Layer #2 and Pooling Layer #2
-        conv2 = tf.layers.conv2d(
-              inputs=pool1,
-              filters=64,
-              kernel_size=[5, 5],
-              padding="same",
-              activation=tf.nn.relu)
-        pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
-        
-          # Dense Layer
-        pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
-        dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
-        dropout = tf.layers.dropout(
-              inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
-        
-          # Logits Layer
-        logits = tf.layers.dense(inputs=dropout, units=output_nodes_number)
-        
-        predicted_classes =tf.argmax(input=logits, axis=1)
-        predictions = {
-                    'class_ids': predicted_classes[:, tf.newaxis],
-                    'probabilities': tf.nn.softmax(logits, name="softmax_tensor"),
-                    'logits': logits,
-                }
-        export_outputs = {
-          'prediction': tf.estimator.export.PredictOutput(predictions)
-          }
-        if mode == tf.estimator.ModeKeys.PREDICT:  
-            return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions, export_outputs=export_outputs)
-        
-          # Calculate Loss (for both TRAIN and EVAL modes)
-        loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
-        
-          # Configure the Training Op (for TRAIN mode)
-        if mode == tf.estimator.ModeKeys.TRAIN:
-            optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
-            train_op = optimizer.minimize(
-                loss=loss,
-                global_step=tf.train.get_global_step())
-            return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
-        
-          # Add evaluation metrics (for EVAL mode)
-        eval_metric_ops = {
-              "accuracy": tf.metrics.accuracy(
-                  labels=labels, predictions=predictions["class_ids"])
-        }
-        return tf.estimator.EstimatorSpec(
-              mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+    if model_type == "lower":
+        #tensorflow model function for lowerNN
+        def cnn_model(features, labels, mode):
+            
+            input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])
+            
+            # Convolutional Layer #1
+            conv1 = tf.layers.conv2d(
+                  inputs=input_layer,
+                  filters=32,
+                  kernel_size=[5, 5],
+                  padding="same",
+                  activation=tf.nn.relu)
+            
+              # Pooling Layer #1
+            pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+            
+              # Convolutional Layer #2 and Pooling Layer #2
+            conv2 = tf.layers.conv2d(
+                  inputs=pool1,
+                  filters=64,
+                  kernel_size=[5, 5],
+                  padding="same",
+                  activation=tf.nn.relu)
+            pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+            
+              # Dense Layer
+            pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
+            dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+            dropout = tf.layers.dropout(
+                  inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
+            
+              # Logits Layer
+            logits = tf.layers.dense(inputs=dropout, units=output_nodes_number)
+            
+            predicted_classes =tf.argmax(input=logits, axis=1)
+            predictions = {
+                        'class_ids': predicted_classes[:, tf.newaxis],
+                        'probabilities': tf.nn.softmax(logits, name="softmax_tensor"),
+                        'logits': logits,
+                    }
+            export_outputs = {
+              'prediction': tf.estimator.export.PredictOutput(predictions)
+              }
+            if mode == tf.estimator.ModeKeys.PREDICT:  
+                return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions, export_outputs=export_outputs)
+            
+              # Calculate Loss (for both TRAIN and EVAL modes)
+            loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+            
+              # Configure the Training Op (for TRAIN mode)
+            if mode == tf.estimator.ModeKeys.TRAIN:
+                optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+                train_op = optimizer.minimize(
+                    loss=loss,
+                    global_step=tf.train.get_global_step())
+                return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+            
+              # Add evaluation metrics (for EVAL mode)
+            eval_metric_ops = {
+                  "accuracy": tf.metrics.accuracy(
+                      labels=labels, predictions=predictions["class_ids"])
+            }
+            return tf.estimator.EstimatorSpec(
+                  mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
     
-    #tensorflow model function for upperNN
-    def cnn_model_upper(features, labels, mode):
-        
-        input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])
-        
-        # Convolutional Layer #1
-        conv1 = tf.layers.conv2d(
-              inputs=input_layer,
-              filters=32,
-              kernel_size=[5, 5],
-              padding="same",
-              activation=tf.nn.L)
-        
-          # Pooling Layer #1
-        pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
-        
-          # Convolutional Layer #2 and Pooling Layer #2
-        conv2 = tf.layers.conv2d(
-              inputs=pool1,
-              filters=64,
-              kernel_size=[5, 5],
-              padding="same",
-              activation=tf.nn.L)
-        pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
-        
-          # Dense Layer
-        pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
-        dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.L)
-        dropout = tf.layers.dropout(
-              inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
-        
-          # Logits Layer
-        logits = tf.layers.dense(inputs=dropout, units=output_nodes_number)
-        
-        predicted_classes =tf.argmax(input=logits, axis=1)
-        predictions = {
-                    'class_ids': predicted_classes[:, tf.newaxis],
-                    'probabilities': tf.nn.softmax(logits, name="softmax_tensor"),
-                    'logits': logits,
-                }
-        export_outputs = {
-          'prediction': tf.estimator.export.PredictOutput(predictions)
-          }
-        if mode == tf.estimator.ModeKeys.PREDICT:  
-            return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions, export_outputs=export_outputs)
-        
-          # Calculate Loss (for both TRAIN and EVAL modes)
-        loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
-        
-          # Configure the Training Op (for TRAIN mode)
-        if mode == tf.estimator.ModeKeys.TRAIN:
-            optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
-            train_op = optimizer.minimize(
-                loss=loss,
-                global_step=tf.train.get_global_step())
-            return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
-        
-          # Add evaluation metrics (for EVAL mode)
-        eval_metric_ops = {
-              "accuracy": tf.metrics.accuracy(
-                  labels=labels, predictions=predictions["class_ids"])
-        }
-        return tf.estimator.EstimatorSpec(
-              mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+    if model_type == "upper":
+        #tensorflow model function for upperNN
+        def cnn_model(features, labels, mode):
+            
+            input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])
+            
+            # Convolutional Layer #1
+            conv1 = tf.layers.conv2d(
+                  inputs=input_layer,
+                  filters=32,
+                  kernel_size=[5, 5],
+                  padding="same",
+                  activation=tf.nn.L)
+            
+              # Pooling Layer #1
+            pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+            
+              # Convolutional Layer #2 and Pooling Layer #2
+            conv2 = tf.layers.conv2d(
+                  inputs=pool1,
+                  filters=64,
+                  kernel_size=[5, 5],
+                  padding="same",
+                  activation=tf.nn.L)
+            pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+            
+              # Dense Layer
+            pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
+            dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.L)
+            dropout = tf.layers.dropout(
+                  inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
+            
+              # Logits Layer
+            logits = tf.layers.dense(inputs=dropout, units=output_nodes_number)
+            
+            predicted_classes =tf.argmax(input=logits, axis=1)
+            predictions = {
+                        'class_ids': predicted_classes[:, tf.newaxis],
+                        'probabilities': tf.nn.softmax(logits, name="softmax_tensor"),
+                        'logits': logits,
+                    }
+            export_outputs = {
+              'prediction': tf.estimator.export.PredictOutput(predictions)
+              }
+            if mode == tf.estimator.ModeKeys.PREDICT:  
+                return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions, export_outputs=export_outputs)
+            
+              # Calculate Loss (for both TRAIN and EVAL modes)
+            loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+            
+              # Configure the Training Op (for TRAIN mode)
+            if mode == tf.estimator.ModeKeys.TRAIN:
+                optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+                train_op = optimizer.minimize(
+                    loss=loss,
+                    global_step=tf.train.get_global_step())
+                return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+            
+              # Add evaluation metrics (for EVAL mode)
+            eval_metric_ops = {
+                  "accuracy": tf.metrics.accuracy(
+                      labels=labels, predictions=predictions["class_ids"])
+            }
+            return tf.estimator.EstimatorSpec(
+                  mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
         
     def serving_input_receiver_fn():
       inputs = {
@@ -241,7 +243,7 @@ test_data = test_data.astype('float32') /255
 
 #this line creates and trains the entire model the inputs are below and must be given 
 # an output number of nodes, for this there is 10
-model_location = CNN(train_data,train_labels,eval_data,eval_labels,10, "save")
+model_location = CNN(train_data,train_labels,eval_data,eval_labels,10, "save", "lower")
 
 #this is how we can load a specific model and then predict on that model
 sess = tf.Session()
