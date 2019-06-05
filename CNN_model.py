715333683c4a9fdb183dde3b199e8835eb52d9a5
Jas10022 <jas10022jb@gmail.com>
2019-06-05 13:08:42 -0700

    clean up

--- CNN_model.py
+++ CNN_model.py
@@ -260,101 +260,6 @@ def cnn_model_test(features, labels, mode):
             }
             return tf.estimator.EstimatorSpec(
                   mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
-    
-
-
-# example of how to implement
-# make sure when we group all the training data that the data is seperated into 
-# training and test data to make sure the model is accurate on a new set of data that
-# the neural network has not see yet
-
-#this data is already in 28 x 28 format so it did not need to resize the pictures
-#also this data is also gray scaled already so we dont need to apply it to this either
-#retrieving the data
-((train_data, train_labels),
- (eval_data, eval_labels)) = tf.keras.datasets.mnist.load_data()
-
-train_data = train_data/np.float32(255)
-train_labels = train_labels.astype(np.int32)  # not required
-
-eval_data = eval_data/np.float32(255)
-eval_labels = eval_labels.astype(np.int32)  # not required
-
-test_data = pd.read_csv("test.csv")
-
-test_data = test_data.values.reshape((-1, 28, 28, 1))
-test_data = test_data.astype('float32') /255
-
-#this line creates and trains the entire model the inputs are below and must be given 
-# an output number of nodes, for this there is 10
-model_location = CNN(train_data,train_labels,eval_data,eval_labels,10, "save", "lower")
-
-#Bellow is how to train the UpperNN model for the catagories, I gave the example for the Kingdom
-
-#this reads the first file of image data
-#each of the files have 25000 images except for the last one
-pkl_file = open('54.pkl', 'rb')
-data1 = pickle.load(pkl_file)
-pkl_file.close()
-
-#This reads the Label data and extracts the specific values from the file read above
-upperNN = pd.read_csv('Data/upperNN.csv')
-Labels = upperNN['Kingdom'][0:25000]
-
-
-#this will run and create the model in the local working directory
-model_location = CNN(X_train,y_train,X_val,y_val,1, "Kingdom_Model", "upper")
-
-cat = "Kingdom_Model"
-with tf.Session() as sess:
-          # Restore variables from disk.
-    saver = tf.train.import_meta_graph(cat + "/model.ckpt-" + str(b * 10) + ".meta")
-    saver.restore(sess, cat + "/model.ckpt-" + str(b * 10))
-    print("Model restored.")
-          
-    sunspot_classifier = tf.estimator.Estimator(
-    model_fn=cnn_model_test, model_dir=cat)
-        
-        # Set up logging for predictions
-        # Log the values in the "Softmax" tensor with label "probabilities"
-    tensors_to_log = {"probabilities": "softmax_tensor"}
-    logging_hook = tf.train.LoggingTensorHook(
-        tensors=tensors_to_log, every_n_iter=50)
-          
-          ## train here
-    train_input_fn = tf.estimator.inputs.numpy_input_fn(
-                   x={"x": X_train},
-                   y=y_train,
-                   batch_size=100,
-                   num_epochs=None,
-                   shuffle=True)
-
-        # Train one step and display the probabilties
-    sunspot_classifier.train(
-            input_fn=train_input_fn,
-            steps=1,
-            hooks=[logging_hook])
-        #change steps to 20000
-    sunspot_classifier.train(input_fn=train_input_fn, steps=10)
-    
-        # Evaluation of the neural network
-    eval_input_fn = tf.estimator.inputs.numpy_input_fn(
-            x={"x": X_val},
-            y=y_val,
-            num_epochs=1,
-            shuffle=False)
-    
-    eval_results = sunspot_classifier.evaluate(input_fn=eval_input_fn)
-    print(eval_results)
-
- # predict with the model and print results
- pred_input_fn = tf.estimator.inputs.numpy_input_fn(
-    x={"x": X_val},
-    shuffle=False)
-    pred_results = sunspot_classifier.predict(input_fn=pred_input_fn)
-     
-    pred_class = np.array([p['class_ids'] for p in pred_results]).squeeze()
-    print(pred_class)
 
         
 
@@ -448,4 +353,6 @@ for cat in col_names:
             print(eval_results)
     if b == 11:
         break
+    
+#this will be the lower NN code to run
    
